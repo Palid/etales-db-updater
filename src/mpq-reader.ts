f9b279f1ca1c52fba2b7536bf8f352206df70bed
Dariusz Niemczyk <palid@phoelid.me>
2021-06-08 14:45:21 +0200

    Make reader properly take the path

--- src/mpq-reader.ts
+++ src/mpq-reader.ts
@@ -1,36 +1,39 @@
 // import * as war3map from "war3map";
-import { resolve } from "path";
+import { dirname, basename, sep } from "path";
 
 import { TextDecoder } from "util";
 
 const { FS, MPQ } = require("@wowserhq/stormjs");
 
-const stormPath = resolve(__dirname, "../../stormjs");
-
 const files = {
   strings: "war3map.wts",
   jass: "war3map.j",
+  items: "war3map.w3t",
 };
 
-export async function read() {
+export async function read(pathToMap: string) {
+  const dir = dirname(pathToMap);
+  const fileName = basename(pathToMap);
   FS.mkdir("/stormjs");
-  FS.mount(FS.filesystems.NODEFS, { root: stormPath }, "/stormjs");
-  const results = new Map<string, string>();
-  const decoder = new TextDecoder("utf-8");
-  const mpq = await MPQ.open("/stormjs/mapfile/etales1.3.4fix.w3x", "r");
+  FS.mount(FS.filesystems.NODEFS, { root: dir }, `${sep}${fileName}`);
+  const results = new Map<string, Uint8Array>();
+  // This __should__ work on windows, but need to double check it.
+  const mpq = await MPQ.open(`/stormjs/${dirname}/${fileName}`, "r");
   for (const [name, fileName] of Object.entries(files)) {
     const file = mpq.openFile(fileName);
     const data = file.read();
-    results.set(name, decoder.decode(data));
+    // Copy the data, as the library returns the same Uint8Array object
+    results.set(name, data.slice(0));
     file.close();
   }
   mpq.close();
   FS.unmount("/stormjs");
   FS.rmdir("/stormjs");
 
+  const decoder = new TextDecoder("utf-8");
   const ret = {
-    strings: results.get("strings") || "",
-    jass: results.get("jass") || "",
+    strings: decoder.decode(results.get("strings")),
+    jass: decoder.decode(results.get("jass")),
   };
   return ret;
 }
