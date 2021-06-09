d9338b5e87c492e8855d12bb5361c6dc09dbf335
Dariusz Niemczyk <palid@phoelid.me>
2021-06-09 21:23:24 +0200

    Make mpq-reader provide all data necessary

--- src/mpq-reader.ts
+++ src/mpq-reader.ts
@@ -1,5 +1,5 @@
-// import * as war3map from "war3map";
-import { dirname, basename, sep } from "path";
+import * as war3map from "war3map";
+import { dirname, basename } from "path";
 
 import { TextDecoder } from "util";
 
@@ -9,16 +9,17 @@ const files = {
   strings: "war3map.wts",
   jass: "war3map.j",
   items: "war3map.w3t",
+  units: "war3map.w3u",
 };
 
 export async function read(pathToMap: string) {
   const dir = dirname(pathToMap);
   const fileName = basename(pathToMap);
   FS.mkdir("/stormjs");
-  FS.mount(FS.filesystems.NODEFS, { root: dir }, `${sep}${fileName}`);
+  FS.mount(FS.filesystems.NODEFS, { root: dir }, `/stormjs/`);
   const results = new Map<string, Uint8Array>();
   // This __should__ work on windows, but need to double check it.
-  const mpq = await MPQ.open(`/stormjs/${dirname}/${fileName}`, "r");
+  const mpq = await MPQ.open(`/stormjs/${fileName}`, "r");
   for (const [name, fileName] of Object.entries(files)) {
     const file = mpq.openFile(fileName);
     const data = file.read();
@@ -31,9 +32,18 @@ export async function read(pathToMap: string) {
   FS.rmdir("/stormjs");
 
   const decoder = new TextDecoder("utf-8");
+  const items = Buffer.from(results.get("items") ?? new Uint8Array());
+  const war3Items = new war3map.ObjectsObject(false);
+  war3Items.read(items);
+  const units = Buffer.from(results.get("units") ?? new Uint8Array());
+  const war3Units = new war3map.ObjectsObject(false);
+  war3Units.read(units);
+
   const ret = {
     strings: decoder.decode(results.get("strings")),
     jass: decoder.decode(results.get("jass")),
+    items: war3Items,
+    units: war3Units,
   };
   return ret;
 }
